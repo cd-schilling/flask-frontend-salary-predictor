8c5810e7b3f0999c3ab51135577bc966d3ce2ad0
Cade Schilling <cdschilling@uiowa.edu>
2025-05-03 13:50:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask import Flask
 
 app = Flask(__name__)
-CORS(app)
 
-@app.route("/predict", methods=["POST"])
-def predict():
-    return jsonify({"message": "API is working!"})
+@app.route("/")
+def hello():
+    return "Hello from Flask on Azure!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()

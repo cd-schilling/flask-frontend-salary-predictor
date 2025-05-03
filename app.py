8f4a7ce975149196aa4f8d2753f33e1cfdc2816a
Cade Schilling <cdschilling@uiowa.edu>
2025-05-03 16:37:06 -0500

    Add requirements.txt for Azure deployment

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "✅ Flask frontend is running on Azure!"
+api_url = "https://salary-api-cdschilling-duanedh7cbdehfecm.centralus-01.azurewebsites.net/predict"
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    prediction = None
+    if request.method == "POST":
+        education = request.form["education"]
+        experience = request.form["experience"]
+
+        response = requests.post(api_url, json={
+            "education": education,
+            "experience": int(experience)
+        })
+
+        if response.status_code == 200:
+            prediction = response.json().get("prediction")
+        else:
+            prediction = "Error getting prediction."
+
+    return render_template("index.html", prediction=prediction)
